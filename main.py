5579755a2ccf213c88932deed6c177b596671746
NuckyDuck <87288180+NuckyDuck@users.noreply.github.com>
2024-06-17 23:14:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ url = "/tells/new"
 mensajes = 0
 retries = 0
 
+tokens = [
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA1MzcxMTk2LCJpYXQiOjE3MTg2NzkxNzB9.eLb8oVjhHObcCU_Srr5x7nbyDkqTjlOChBUX8Dp-P_E",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA1MzY2NzIzLCJpYXQiOjE3MTg2NjY1MTZ9.yRPb5hS1hSLc9TSdgcYMNDUGRaseh4V1d5aNd84QlnA",
+]
+
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
@@ -35,14 +40,11 @@ dic_msg = [
 
 while True:
     diccionario = random.choice(dic_msg)
-
+    token = random.choice(tokens)
     payload = json.dumps({
         "isInstagramInAppBrowser": False,
         "isSnapchatInAppBrowser": False,
         "isSenderRevealed": False,
-        "limit": 13,
-        "referalId": 154,
-        "referralType": "dice",
         "tell": diccionario,
         "userId": 105347342,
         "username": "funa.luismadina"
@@ -53,6 +55,7 @@ while True:
         "Accept": "application/json",
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "Accept-Language": "es-ES,es;q=0.7",
+        "Authorization": token,
         "Content-Type": "application/json;charset=utf-8",
         "Origin": "https://tellonym.me",
         "Referer": "https://tellonym.me/",
@@ -72,25 +75,34 @@ while True:
         conn.request("POST", url, body=payload, headers=headers)
 
         response = conn.getresponse()
+        content_type = response.getheader('Content-Type')
         data = response.read()
 
         if response.status == 200:
             mensajes += 1
-            print(f"[+] Mensaje enviado. Total mensajes enviados: {mensajes} - {diccionario} - {user_agent}")
-            time.sleep(5)
+            print(f"[+] Mensaje enviado. Total mensajes enviados: {mensajes} - {diccionario} - {token}")
+            time.sleep(3)
         elif response.status == 429:
             retries += 1
             print(f"[!] Límite de peticiones excedido. Esperando 20 segundos...")
             time.sleep(20)
         else:
-            print(f"[!] Error: Estado de respuesta: {response.status}, Mensaje: {data.decode('utf-8')}")
+            if content_type and 'application/json' in content_type:
+                try:
+                    message = data.decode('utf-8')
+                    print(f"[!] Error: Estado de respuesta: {response.status}, Mensaje: {message}")
+                except UnicodeDecodeError as e:
+                    print(f"[!] Error al decodificar la respuesta: {e}")
+                    print(f"[!] Respuesta cruda: {data}")
+            else:
+                print(f"[!] Error: Estado de respuesta: {response.status}, Datos no son JSON: {data}")
 
         conn.close()
     except Exception as e:
         print(f"[!] Error de conexión: {e}")
 
-    if mensajes >= 50:
-        print("[!] Alcanzado el límite de 50 mensajes. Esperando 5 minutos antes de continuar...")
+    if retries >= 20:
+        print("[!] Alcanzado el límite de 20 re-peticiones. Esperando 5 minutos antes de continuar...")
         time.sleep(300)
         retries = 0
 
